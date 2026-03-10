cb0250ec8bb35f177962be7360290e81e4ade9ec
semih702 <semih702do@gmail.com>
2026-03-10 15:35:29 +0300

    terraform: use IRSA for EBS CSI addon

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+  }
+}
+
 resource "aws_iam_role" "cluster" {
   name = "${var.cluster_name}-cluster-role"
 
@@ -70,6 +79,51 @@ resource "aws_iam_role_policy_attachment" "node_ebs_csi_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint
+  ]
+
+  tags = merge(var.tags, {
+    Name = "${var.cluster_name}-oidc-provider"
+  })
+}
+
+resource "aws_iam_role" "ebs_csi_irsa" {
+  name = "${var.cluster_name}-ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${local.oidc_issuer_hostpath}:aud" = "sts.amazonaws.com"
+            "${local.oidc_issuer_hostpath}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = merge(var.tags, {
+    Name = "${var.cluster_name}-ebs-csi-role"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_irsa_policy" {
+  role       = aws_iam_role.ebs_csi_irsa.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
 resource "aws_eks_cluster" "this" {
   name     = var.cluster_name
   role_arn = aws_iam_role.cluster.arn
@@ -134,6 +188,7 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   cluster_name                = aws_eks_cluster.this.name
   addon_name                  = "aws-ebs-csi-driver"
   addon_version               = data.aws_eks_addon_version.ebs_csi_driver.version
+  service_account_role_arn    = aws_iam_role.ebs_csi_irsa.arn
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 
@@ -142,7 +197,8 @@ resource "aws_eks_addon" "ebs_csi_driver" {
   })
 
   depends_on = [
-    aws_eks_node_group.this
+    aws_eks_node_group.this,
+    aws_iam_role_policy_attachment.ebs_csi_irsa_policy
   ]
 }
 
@@ -150,4 +206,12 @@ data "aws_eks_addon_version" "ebs_csi_driver" {
   addon_name         = "aws-ebs-csi-driver"
   kubernetes_version = var.cluster_version
   most_recent        = true
+}
+
+data "tls_certificate" "eks_oidc" {
+  url = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+locals {
+  oidc_issuer_hostpath = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
 }
