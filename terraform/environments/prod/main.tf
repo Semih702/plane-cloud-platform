fae7e606ffb9abb53626d8b515c1a753946d50e9
WhiteRow33 <yunusemrekart0142@gmail.com>
2026-05-26 23:16:56 +0300

    Fix Plane S3 upload redirects

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -163,6 +163,18 @@ resource "aws_s3_bucket_public_access_block" "plane_docstore" {
   restrict_public_buckets = true
 }
 
+resource "aws_s3_bucket_cors_configuration" "plane_docstore" {
+  bucket = aws_s3_bucket.plane_docstore.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET", "HEAD", "POST", "PUT"]
+    allowed_origins = var.plane_docstore_cors_allowed_origins
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
 resource "aws_iam_role" "plane_irsa" {
   name = "${var.project_name}-${var.environment}-plane-irsa-role"
 
